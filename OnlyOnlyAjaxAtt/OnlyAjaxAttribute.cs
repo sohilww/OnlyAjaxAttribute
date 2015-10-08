c3ec9cb2629c323b9c5412be818aba58eaf756a1
Soheil Karami <sohil_ww@ymail.com>
2015-10-08 23:55:12 +0330

    Create Repository
    Create Repository

---
+++ OnlyOnlyAjaxAtt/OnlyAjaxAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+namespace OnlyOnlyAjaxAtt
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class OnlyAjaxAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if(filterContext.HttpContext.Request.IsAjaxRequest())
+              base.OnActionExecuting(filterContext);
+            else
+            {
+                throw new InvalidOperationException("This operation can only be accessed via Ajax requests");
+            }
+        }
+    }
+}
